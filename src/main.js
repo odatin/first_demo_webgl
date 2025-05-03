02766ae4ee2e7cb3a5c721b1fa773dce4b4aa061
taku <odatin.tyoudai@gmail.com>
2025-05-03 20:25:32 +0900

    リサイズに対応、アニメーションの追加

--- src/main.js
+++ src/main.js
@@ -19,3 +19,22 @@ const camera = new THREE.PerspectiveCamera(
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 });
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(window.devicePixelRatio);
+
+
+const tick = () => {
+  window.requestAnimationFrame(tick);
+  renderer.render(scene, camera);
+};
+
+tick();
+
+window.addEventListener('reseize', () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(window.devicePixelRatio);
+});
