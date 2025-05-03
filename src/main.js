bc716d09064a54de73e89510a4d371c9bc8aaa57
taku <odatin.tyoudai@gmail.com>
2025-05-03 19:06:44 +0900

    three.jsの読み込み import * as THREE from 'three'

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import './style.css';
+import * as THREE from 'three';
+
+console.log(THREE);
 
